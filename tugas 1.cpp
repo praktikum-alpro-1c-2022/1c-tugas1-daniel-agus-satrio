d597550380069e93d36cc10f53e1c74c353882cb
DanielAgusSatrio <114908515+DanielAgusSatrio@users.noreply.github.com>
2022-10-09 20:10:34 +0700

    Tugas alpro 1 (no 1 dan 2)

---
+++ tugas 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string nama = "Daniel Agus Satrio";
+    string kelas = "1C Informatika";
+    string hobby = "Menyanyi";
+    string npm = "2210631170017";
+    string cita = "Menjadi orang kaya";
+    string umur = "19 Tahun";
+
+    cout<< "Nama saya adalah "<< nama<< endl;
+    cout<< "Saya kelas "<< kelas<< endl;
+    cout<< "Npm : "<< npm<< endl;
+    cout<< "Umur saya adalah "<< umur<< endl;
+    cout<< "Cita-cita saya adalah "<< cita<<endl;
+
+
+}
